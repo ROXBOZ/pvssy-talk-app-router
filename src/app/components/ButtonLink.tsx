217fd16e67a26b3d27ee4f37c3967f1aaa7d1250
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 16:06:59 +0200

    creat ButtonLink

---
+++ src/app/components/ButtonLink.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+function ButtonLink() {
+  return (
+    <Link className="bg-gray-300 rounded-full px-4 py-2 w-fit" href="">
+      ButtonLink
+    </Link>
+  );
+}
+
+export default ButtonLink;
