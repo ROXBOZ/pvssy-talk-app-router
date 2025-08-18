3fdfef481d25a9464bdc97bb8b4f8de3a1634715
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 17:13:55 +0200

    responsive fix

--- src/app/components/Header/HomeScreen.tsx
+++ src/app/components/Header/HomeScreen.tsx
@@ -5,9 +5,9 @@ function HomeScreen() {
   return (
     <div className="flex h-[95vh] flex-col bg-violet-300">
       <HeaderBar />
-      <div className="flex flex-1 items-center justify-center p-4">
+      <div className="flex flex-1 flex-col items-center justify-center p-4 lg:flex-row">
         <div className="flex-1">img</div>
-        <h1 className="font-heading flex-1 text-8xl">
+        <h1 className="font-heading flex-1 text-6xl lg:text-8xl">
           avoir mal n’est pas normal.
         </h1>
       </div>
