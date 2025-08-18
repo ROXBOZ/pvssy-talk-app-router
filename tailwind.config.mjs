52e7fa8d4b61848e9c2c62a7a886db0fed6c79d8
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 17:01:46 +0200

    setting tailwind (can't use custom colors) to be fixed

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/app/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: {
+          300: "#e09cff",
+        },
+      },
+    },
+  },
+};
