f2adbde93ebcf5a5d2c7060fc75239962e0cbfab
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 17:07:00 +0200

    add color

--- src/app/components/Homepage/ArticlesDirectory/ArticleFilters.tsx
+++ src/app/components/Homepage/ArticlesDirectory/ArticleFilters.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Filters() {
-  return <div className="bg-gray-300 p-4">Filters</div>;
+  return <div className="bg-gray-200 p-4">Filters</div>;
 }
 
 export default Filters;
