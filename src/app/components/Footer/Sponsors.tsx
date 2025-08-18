81259ba501b7ef2aaee39298cc0c1907f757a039
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 17:05:15 +0200

    update style

--- src/app/components/Footer/Sponsors.tsx
+++ src/app/components/Footer/Sponsors.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Sponsors() {
   return (
-    <div className="py-8">
-      <h2 className="p-4 text-3xl font-heading">Sponsors</h2>
+    <div className="py-24">
+      <h2 className="font-heading p-4 text-2xl">Sponsors</h2>
       <div className="customGrid">
         <div className="bg-gray-100">Logo</div>
         <div className="bg-gray-100">Logo</div>
