1cd2eb525b5c7717b4be4fc8b57d50b68977cfea
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 01:39:22 +0200

    added shadow, rounded, color

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 function Card({ showButton }: { showButton: boolean }) {
   return (
-    <div className="col-span-2 w-full bg-gray-200">
-      <div className="aspect-video bg-gray-300">img</div>
+    <div className="col-span-2 w-full overflow-hidden rounded-2xl bg-orange-50 shadow shadow-orange-100">
+      <div className="aspect-video bg-orange-100 p-4">img</div>
       <div className="flex items-baseline justify-between gap-4 p-4">
         <h3 className="font-heading text-xl">Title</h3>
         {showButton && <ButtonLink hasArrow={true} />}
