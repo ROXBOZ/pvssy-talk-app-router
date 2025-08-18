767b07103c30617a25e319bd6edeaeeee544eff4
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 17:04:43 +0200

    added option for arrow

--- src/app/components/ButtonLink.tsx
+++ src/app/components/ButtonLink.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import React from "react";
 
-function ButtonLink() {
+function ButtonLink({ hasArrow }: { hasArrow: boolean }) {
   return (
-    <Link className="bg-gray-300 rounded-full px-4 py-2 w-fit" href="">
-      ButtonLink
+    <Link
+      className="w-fit rounded-full bg-violet-300 px-4 py-2 ring-violet-400 ring-inset hover:ring-2 hover:delay-200 hover:duration-200 active:bg-violet-400"
+      href=""
+    >
+      Label title
+      {hasArrow && <span className="ml-2">→</span>}
     </Link>
   );
 }
