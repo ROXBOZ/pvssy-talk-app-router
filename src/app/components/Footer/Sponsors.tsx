155a5adacead5a640bc369041f808ff569581174
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 01:39:31 +0200

    fix css

--- src/app/components/Footer/Sponsors.tsx
+++ src/app/components/Footer/Sponsors.tsx
@@ -2,18 +2,18 @@ import React from "react";
 
 function Sponsors() {
   return (
-    <div className="py-24">
+    <div className="pt-96 pb-12">
       <h2 className="font-heading p-4 text-2xl">Sponsors</h2>
       <div className="customGrid">
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
-        <div className="bg-gray-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
+        <div className="bg-violet-100">Logo</div>
       </div>
     </div>
   );
