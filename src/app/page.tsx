7ad5b16f7216ee5acb012e17318ddb3b97a48978
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 16:09:42 +0200

    create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import ArticlesDirectory from "./components/Homepage/ArticlesDirectory/ArticlesDirectory";
+import PageSuggestionScreen from "./components/Homepage/PageSuggestionScreen";
+import PagesDirectory from "./components/Homepage/PagesDirectory";
+import StatsScreen from "./components/Homepage/StatsScreen";
+
 export default function Home() {
-  return <div></div>;
+  //NOTE Homescreen available in Header
+  return (
+    <div className="flex flex-col gap-24">
+      <StatsScreen />
+      <ArticlesDirectory />
+      <PageSuggestionScreen />
+      <PagesDirectory />
+      <PageSuggestionScreen />
+      <PagesDirectory />
+    </div>
+  );
 }
