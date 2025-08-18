41d6f8984123442db5a1b86bca6e382c227f9958
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-18 17:05:43 +0200

    update color

--- src/app/components/Header/HeaderNav.tsx
+++ src/app/components/Header/HeaderNav.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function HeaderNav() {
-  return <div className="col-start-4 col-end-7 bg-gray-300 p-4">HeaderNav</div>;
+  return (
+    <div className="col-start-4 col-end-7 bg-violet-400 p-4">HeaderNav</div>
+  );
 }
 
 export default HeaderNav;
