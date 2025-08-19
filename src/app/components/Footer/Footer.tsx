def60c9794174ac8cc206bd3e7d9ab7e87f95ac1
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 10:49:58 +0200

    edit style

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <>
       <Sponsors />
-      <footer className="flex flex-col justify-between gap-12 bg-violet-300 p-4 py-12 md:flex-row">
+      <footer className="bg-primary-300 flex flex-col justify-between gap-12 p-4 py-12 md:flex-row">
         <div>
           <h2 className="font-heading text-2xl">Collab</h2>
           <h2 className="font-heading text-2xl">Newsletter</h2>
