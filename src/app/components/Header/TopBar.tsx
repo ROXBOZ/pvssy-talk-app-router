d1e3480206d1c7b140acd255ef8312dbf3b4de58
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 02:16:53 +0200

    fix fetching

--- src/app/components/Header/TopBar.tsx
+++ src/app/components/Header/TopBar.tsx
@@ -1,16 +1,41 @@
+export const dynamic = "force-static";
+
 import HeaderNav from "./HeaderNav";
 import React from "react";
+import { client } from "../../../../config/sanity";
 
-function TopBar() {
-  return (
-    <header
-      aria-label="Header"
-      className="justify-between items-baseline customGrid"
-    >
-      <div className="font-heading text-2xl">pvssy talk</div>
-      <HeaderNav />
-    </header>
-  );
+interface HeaderNavProps {
+  headerMenu?: {
+    _type: string;
+    title?: string;
+    link?: string;
+    isAction?: boolean;
+    content?: {
+      pages?: { title: string; slug: { current: string } }[];
+    };
+  }[];
 }
 
+const TopBar = async () => {
+  const data: HeaderNavProps | null = await getData();
+
+  return (
+    <header className="customGrid items-baseline justify-between">
+      <div className="font-heading text-2xl">pvssy&nbsp;talk</div>
+      <HeaderNav headerMenu={data?.headerMenu} />
+    </header>
+  );
+};
+
 export default TopBar;
+
+async function getData(): Promise<HeaderNavProps | null> {
+  try {
+    return await client.fetch(
+      '*[_type == "menu" && !(_id in path("drafts.**"))][0]{headerMenu}',
+    );
+  } catch (error) {
+    console.error("Error fetching header menu:", error);
+    return null;
+  }
+}
