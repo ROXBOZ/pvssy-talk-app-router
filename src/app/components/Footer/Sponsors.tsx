b12ab638aae6bbe22f7fd380d8943fde9d86249c
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 02:15:57 +0200

    improve default styling

--- src/app/components/Footer/Sponsors.tsx
+++ src/app/components/Footer/Sponsors.tsx
@@ -5,15 +5,15 @@ function Sponsors() {
     <div className="pt-96 pb-12">
       <h2 className="font-heading p-4 text-2xl">Sponsors</h2>
       <div className="customGrid">
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
-        <div className="bg-violet-100">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
+        <div className="rounded-xl bg-violet-100 p-4">Logo</div>
       </div>
     </div>
   );
