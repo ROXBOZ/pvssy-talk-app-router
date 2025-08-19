ab4f5173b1133cd90dc953fa2b8ccbd8d14dc206
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 17:46:42 +0200

    Fix import path for ButtonLink component and update layout classes in HomeHero for better styling

--- src/app/components/Header/HomeHero.tsx
+++ src/app/components/Header/HomeHero.tsx
@@ -1,4 +1,4 @@
-import ButtonLink from "../UIElements/ButtonLink";
+import ButtonLink from "../UIElements/ButtonLink/ButtonLink";
 import HeaderBar from "./TopBar"; // client component
 import { client } from "../../../../config/sanity";
 
@@ -13,11 +13,11 @@ const HomeHero = async () => {
   return (
     <div className="from-primary-400 to-primary-300 flex h-screen flex-col bg-gradient-to-b">
       <HeaderBar />
-      <div className="flex flex-1 flex-col items-center justify-center p-4 lg:flex-row">
+      <div className="customGrid flex-1 flex-col items-center justify-center p-4 lg:flex-row">
         <div className="bg-tertiary-500/30 animation pvssy-size-pulse aspect-square h-full scale-70 rounded-full blur-3xl">
           <div className="animation pvssy-size-pulse bg-tertiary-600 aspect-square h-full scale-70 rounded-full blur-3xl"></div>
         </div>
-        <h1 className="font-heading flex-1 text-6xl lg:text-8xl">
+        <h1 className="font-heading col-start-4 col-end-7 flex-1 text-6xl lg:text-8xl">
           {data.tagline}
         </h1>
       </div>
