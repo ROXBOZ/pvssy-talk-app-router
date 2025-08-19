b9b6f1e0f23b3372852d9f0adffda860a393eb34
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 02:16:44 +0200

    fetch data from sanity

--- src/app/components/Header/HomeScreen.tsx
+++ src/app/components/Header/HomeScreen.tsx
@@ -1,18 +1,38 @@
-import HeaderBar from "../Header/TopBar";
-import React from "react";
+export const dynamic = "force-static";
 
-function HomeScreen() {
+import HeaderBar from "../Header/TopBar";
+import { client } from "../../../../config/sanity";
+
+interface Tagline {
+  tagline?: string;
+}
+
+const HomeScreen = async () => {
+  const data: Tagline | null = await getData();
+  if (!data) return null;
+
   return (
     <div className="flex h-[95vh] flex-col bg-violet-300">
       <HeaderBar />
       <div className="flex flex-1 flex-col items-center justify-center p-4 lg:flex-row">
         <div className="flex-1">img</div>
         <h1 className="font-heading flex-1 text-6xl lg:text-8xl">
-          avoir mal n’est pas normal.
+          {data.tagline}
         </h1>
       </div>
     </div>
   );
-}
+};
 
 export default HomeScreen;
+
+async function getData(): Promise<Tagline | null> {
+  try {
+    return await client.fetch(
+      '*[_type == "homepage" && !(_id in path("drafts.**"))][0]{tagline}',
+    );
+  } catch (error) {
+    console.error("Error fetching tagline:", error);
+    return null;
+  }
+}
