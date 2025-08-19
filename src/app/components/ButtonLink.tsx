16f6b5b739ea5bb0d59d698f19b8910c1a67a917
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 03:17:32 +0200

    fix color button

--- src/app/components/ButtonLink.tsx
+++ src/app/components/ButtonLink.tsx
@@ -13,7 +13,7 @@ function ButtonLink({
 }) {
   return (
     <Link
-      className={`group flex w-fit items-center gap-2 rounded-full ${color === "violet" ? "bg-violet-300 ring-violet-400 active:bg-violet-400" : "bg-lime-300 ring-lime-400 active:bg-lime-400"} px-6 py-2 font-semibold ring-inset hover:ring-2 hover:delay-200 hover:duration-200`}
+      className={`group flex w-fit items-center gap-2 rounded-full ${color === "violet" ? "bg-violet-300 ring-violet-400 active:bg-violet-400" : "bg-lime-200 ring-lime-300 active:bg-lime-300"} px-6 py-2 font-semibold ring-inset hover:ring-2 hover:delay-200 hover:duration-200`}
       href={href}
     >
       {label ? label : "Default Label"}
