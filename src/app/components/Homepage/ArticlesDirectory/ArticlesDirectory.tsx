bf0c2948fa19d39089f74802dfb228fa7c6c35dc
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 10:52:24 +0200

    build component, animation, fetch

--- src/app/components/Homepage/ArticlesDirectory/ArticlesDirectory.tsx
+++ src/app/components/Homepage/ArticlesDirectory/ArticlesDirectory.tsx
@@ -1,21 +1,36 @@
-import ArticlesHeading from "./ArticlesHeading";
-import Card from "../../Card";
-import React from "react";
+import Card, { CardBaseProps } from "../../Card";
 
-function ArticlesDirectory() {
+import ArticlesHeading from "./ArticlesHeading";
+import React from "react";
+import { client } from "../../../../../config/sanity";
+
+interface ArticleCardProps extends CardBaseProps {}
+
+const ArticlesDirectory = async () => {
+  const articles: ArticleCardProps[] | null = await getArticles();
+  if (!articles || articles.length === 0) return null;
+
   return (
     <div>
       <ArticlesHeading />
       <div className="customGrid">
-        <Card showButton={false} />
-        <Card showButton={false} />
-        <Card showButton={false} />
-        <Card showButton={false} />
-        <Card showButton={false} />
-        <Card showButton={false} />
+        {articles.map((article) => (
+          <Card key={article._id} {...article} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
 export default ArticlesDirectory;
+
+async function getArticles(): Promise<ArticleCardProps[] | null> {
+  try {
+    return await client.fetch(
+      '*[_type == "pain" && !(_id in path("drafts.**"))] {..., filters}',
+    );
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+    return null;
+  }
+}
