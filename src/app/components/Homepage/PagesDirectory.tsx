e144bdd8c88e61da13354b4dbf6b9a44e8d041a0
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-19 22:30:59 +0200

    Refactor PagesDirectory component: add TypeScript interface for props and update rendering logic to use dynamic data

--- src/app/components/Homepage/PagesDirectory.tsx
+++ src/app/components/Homepage/PagesDirectory.tsx
@@ -1,19 +1,52 @@
-import Card from "../UIElements/Card";
+import Card, { CardBaseProps } from "../UIElements/Card";
 import React from "react";
 
-function PagesDirectory() {
+export interface PageDirectoryData {
+  title: string;
+  cards: Array<{
+    _id: string;
+    title: string;
+    description: string;
+    image: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+    };
+  }>;
+}
+
+const PagesDirectory: React.FC<{ data: PageDirectoryData }> = ({ data }) => {
+  const cards: CardBaseProps[] = data.cards.map((card) => {
+    return {
+      _id: card._id,
+      title: card.title,
+      description: card.description,
+      mainImage: {
+        ...card.image,
+        asset: {
+          _ref: card.image.asset._id,
+          ...card.image.asset,
+        },
+      },
+      showButton: true,
+    };
+  });
+
   return (
     <section>
       <div className="p-4">
-        <h2 className="font-heading text-3xl">Lorem Ipsum</h2>
+        <h2 className="font-heading text-3xl">{data.title}</h2>
       </div>
       <div className="customGrid">
-        <Card showButton={true} />
-        <Card showButton={true} />
-        <Card showButton={true} />
+        {cards && cards.length > 0 ? (
+          cards.map((card) => <Card key={card._id} {...card} />)
+        ) : (
+          <div>No pages found.</div>
+        )}
       </div>
     </section>
   );
-}
+};
 
 export default PagesDirectory;
