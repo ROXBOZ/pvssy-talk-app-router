ced76f4050f7beaa8a05c55d4ccaae7b8fc7ee99
Roxanne Borloz <hello@roxanne-borloz.net>
2025-08-21 00:39:58 +0200

    Refactor ButtonLink tests:  remove unused tests

--- next/src/app/components/UI/ButtonLink/ButtonLink.test.tsx
+++ next/src/app/components/UI/ButtonLink/ButtonLink.test.tsx
@@ -1,23 +1,22 @@
 import "@testing-library/jest-dom";
 
-import { fireEvent, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
-import React from "react";
 import ButtonLink from "./ButtonLink";
 
 describe("ButtonLink", () => {
-  it("renders as a button when no href is provided", () => {
-    const { getByRole } = render(
-      <ButtonLink label="Test" color="primary" onClick={() => {}} />,
-    );
-    expect(getByRole("button")).toBeInTheDocument();
-  });
-
   it("renders as a link when href is provided", () => {
     const { getByRole } = render(
       <ButtonLink label="Test Link" color="secondary" href="/test" />,
     );
     expect(getByRole("link")).toBeInTheDocument();
+  });
+
+  it("renders as a button when no href is provided", () => {
+    const { getByRole } = render(
+      <ButtonLink label="Test" color="primary" onClick={() => {}} />,
+    );
+    expect(getByRole("button")).toBeInTheDocument();
   });
 
   it("shows disabled state for button", () => {
@@ -37,50 +36,5 @@ describe("ButtonLink", () => {
       />,
     );
     expect(getByRole("link")).toHaveAttribute("aria-disabled", "true");
-  });
-
-  it("calls onClick when button is clicked", () => {
-    const onClick = jest.fn();
-    const { getByRole } = render(
-      <ButtonLink label="Clickable" color="primary" onClick={onClick} />,
-    );
-    fireEvent.click(getByRole("button"));
-    expect(onClick).toHaveBeenCalled();
-  });
-
-  it("shows arrow in correct direction (left)", () => {
-    const { getByText } = render(
-      <ButtonLink
-        label="Arrow Left"
-        color="primary"
-        hasArrow
-        arrowDirection="left"
-      />,
-    );
-    expect(getByText("←")).toBeInTheDocument();
-  });
-
-  it("shows arrow in correct direction (right)", () => {
-    const { getByText } = render(
-      <ButtonLink
-        label="Arrow Right"
-        color="primary"
-        hasArrow
-        arrowDirection="right"
-      />,
-    );
-    expect(getByText("→")).toBeInTheDocument();
-  });
-
-  it("shows arrow in correct direction (bottom)", () => {
-    const { getByText } = render(
-      <ButtonLink
-        label="Arrow Bottom"
-        color="primary"
-        hasArrow
-        arrowDirection="bottom"
-      />,
-    );
-    expect(getByText("↓")).toBeInTheDocument();
   });
 });
